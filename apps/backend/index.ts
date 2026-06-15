ec4f7f00f616ce5acf2f1d6ec3024c57bfe4b6e8
kiralightyagami <crucisgamer98@gmail.com>
2026-06-15 17:19:03 +0530

    skeleton route

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+//todo add auth middleware
+
+app.post("/buy", (req,res) => {
+
+});
+
+app.post("/sell", (req,res) => {
+
+});
+
+app.post("/split", (req,res) => {
+
+});
+
+app.post("/merge", (req,res) => {
+
+});
+
+app.get("/balance", (req,res) => {
+
+});
+
+app.get("/position", (req,res) => {
+
+});
+
+app.post("/history", (req,res) => {
+
+});
+
+app.listen(8080);
